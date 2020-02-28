b3c3f54549a4780842ee6216f85a342d2142022d
dword <era@tut.by>
2020-02-28 13:46:09 +0300

    test

---
+++ wp-content/themes/stark/template-parts/sociallink.php
@@ -0,0 +1,6 @@
+<!--noindex-->
+	<li><a rel="nofollow" href="https://www.vk.com/vitanis" target="_blank" title="Вконтакте"><i class="icon-vk"></i></a></li>
+	<li><a rel="nofollow" href="https://www.facebook.com/VitanisBY" target="_blank" title="Facebook"><i class="icon-facebook"></i></a></li>
+	<li><a rel="nofollow" href="https://www.twitter.com/VitanisBy" target="_blank" title="Twitter"><i class="icon-twitter"></i></a></li>
+	<li><a rel="nofollow" href="https://plus.google.com/+VitanisBy" target="_blank" title="Google+"><i class="icon-googleplus"></i></a></li>
+<!--/noindex-->
